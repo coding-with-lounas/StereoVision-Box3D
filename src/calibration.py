a0173cab897d5481d515bc1482f9fca690d9ad95
coding-with-lounas <lounasidjr@gmail.com>
2026-04-03 00:15:18 +0100

    add save corner_detection

--- src/calibration.py
+++ src/calibration.py
@@ -1,7 +1,12 @@
 import numpy as np
 import cv2 as cv
 import glob
+import os
 
+output_folder = '../output/corners_detection'
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 
 square_size = 25  # Taille du carré en mm
@@ -19,14 +24,25 @@ objp *= square_size
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('../data/calibration/*.jpg')
+images = glob.glob('./data/calibration/*.jpg')
+print(len(images))
 # Avant la boucle for
 cv.namedWindow('img', cv.WINDOW_NORMAL)
 cv.resizeWindow('img', 1000, 800) 
+image_size = None
 
 for fname in images:
     img = cv.imread(fname)
+    if img is None:
+        print(f"Impossible de lire : {fname}")
+        continue
+
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    if image_size is None:
+        image_size = gray.shape[::-1]
+        # print(f'image size : {image_size}')
+
 
     # Find the chess board corners
     ret, corners = cv.findChessboardCorners(gray, pattern_size, None)
@@ -37,9 +53,14 @@ for fname in images:
 
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
-
         # Draw and display the corners
         cv.drawChessboardCorners(img, pattern_size, corners2, ret)
+        # --- SAVE at corner_detection ---
+        base_name = os.path.basename(fname) 
+        save_path = os.path.join("./output/corners_detection", "drawchessboard_" + base_name)
+        
+        cv.imwrite(save_path, img) # Sauvegarde l'image avec les dessins
+        # ----------------------------------
         cv.imshow('img', img)
         cv.waitKey(500)
     else:
@@ -50,7 +71,7 @@ cv.destroyAllWindows()
 
 # Clibration
 
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, image_size, None, None)
 
 
 if ret:
